dcde70acb45b5800982ea448b3b0b6190f24693c
OwnDroid <askowndroid@gmail.com>
2015-06-10 15:30:03 +0200

    Fixes
    	@akhilnarang Forgot something???
    
    Change-Id: Ief157791ba81fc9fc8cebf6e649883915c11dbb8
    Signed-off-by: OwnDroid <askowndroid@gmail.com>

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -177,11 +177,11 @@ public class QSTileHolder {
                 resourceName = "ic_qs_navbar_on";
                 stringId = R.string.qs_navbar_tile;
                 break;
-	    case QSConstants.TILE_TRDS
-		resourceName = "ic_qs_trds_on"
-		stringId = R.string.qs_tile_trds;
-		break;
-            default:
+			case QSConstants.TILE_TRDS:
+				resourceName = "ic_qs_trds_on";
+				stringId = R.string.qs_tile_trds;
+				break;
+			default:
                 return null;
         }
 
