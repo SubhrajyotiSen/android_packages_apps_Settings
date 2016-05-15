4bfc1bebb0386e9b5305664ffd7c13818da4403a
Daniel Koman <dankoman30@gmail.com>
2016-05-15 11:12:30 +0200

    Settings: SlimDim - pref to listen on entire screen for touches
    Signed-off-by: OwnDroid <askowndroid@gmail.com>

--- src/com/android/settings/ButtonSettings.java
+++ src/com/android/settings/ButtonSettings.java
@@ -93,6 +93,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private static final String KEY_VOLUME_CONTROL_RING_STREAM = "volume_keys_control_ring_stream";
     private static final String KEY_CAMERA_DOUBLE_TAP_POWER_GESTURE = "camera_double_tap_power_gesture";
     private static final String DIM_NAV_BUTTONS = "dim_nav_buttons";
+    private static final String DIM_NAV_BUTTONS_TOUCH_ANYWHERE = "dim_nav_buttons_touch_anywhere";
     private static final String DIM_NAV_BUTTONS_TIMEOUT = "dim_nav_buttons_timeout";
     private static final String DIM_NAV_BUTTONS_ALPHA = "dim_nav_buttons_alpha";
     private static final String DIM_NAV_BUTTONS_ANIMATE = "dim_nav_buttons_animate";
@@ -157,6 +158,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private SwitchPreference mEnableHwKeys;
     private ColorPickerPreference mNavbarButtonTint;
     private SwitchPreference mDimNavButtons;
+    private SwitchPreference mDimNavButtonsTouchAnywhere;
     private SlimSeekBarPreference mDimNavButtonsTimeout;
     private SlimSeekBarPreference mDimNavButtonsAlpha;
     private SwitchPreference mDimNavButtonsAnimate;
@@ -460,6 +462,9 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         mDimNavButtons = (SwitchPreference) findPreference(DIM_NAV_BUTTONS);
         mDimNavButtons.setOnPreferenceChangeListener(this);
 
+        mDimNavButtonsTouchAnywhere = (SwitchPreference) findPreference(DIM_NAV_BUTTONS_TOUCH_ANYWHERE);
+        mDimNavButtonsTouchAnywhere.setOnPreferenceChangeListener(this);
+
         mDimNavButtonsTimeout = (SlimSeekBarPreference) findPreference(DIM_NAV_BUTTONS_TIMEOUT);
         mDimNavButtonsTimeout.setDefault(3000);
         mDimNavButtonsTimeout.isMilliseconds(true);
@@ -492,6 +497,11 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         if (mDimNavButtons != null) {
             mDimNavButtons.setChecked(Settings.System.getInt(getContentResolver(),
                     Settings.System.DIM_NAV_BUTTONS, 0) == 1);
+        }
+
+        if (mDimNavButtonsTouchAnywhere != null) {
+            mDimNavButtonsTouchAnywhere.setChecked(Settings.System.getInt(getContentResolver(),
+                    Settings.System.DIM_NAV_BUTTONS_TOUCH_ANYWHERE, 0) == 1);
         }
 
         if (mDimNavButtonsTimeout != null) {
@@ -665,6 +675,11 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.NAVIGATION_BAR_SHOW,
                         ((Boolean) newValue) ? 1 : 0);
+            return true;
+        } else if (preference == mDimNavButtonsTouchAnywhere) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.DIM_NAV_BUTTONS_TOUCH_ANYWHERE,
+                    ((Boolean) newValue) ? 1 : 0);
             return true;
         } else if (preference == mEnableHwKeys) {
             boolean hWkeysValue = (Boolean) newValue;
