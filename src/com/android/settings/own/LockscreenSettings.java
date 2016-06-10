2f0107c4c296230f93634144912845cbe75233f9
lion0738 <lion0738@naver.com>
2016-06-10 17:25:50 +0200

    Lockscreen blur & see through (2/2)
    Signed-off-by: OwnDroid <askowndroid@gmail.com>

--- src/com/android/settings/own/LockscreenSettings.java
+++ src/com/android/settings/own/LockscreenSettings.java
@@ -1,36 +1,59 @@
 /*
- * Copyright (C) 2015-2016 OwnROM
+ *  Copyright (C) 2015-2016 OwnROM
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
-
 package com.android.settings.own;
 
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import com.android.internal.logging.MetricsLogger;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.app.WallpaperManager;
+import android.content.Intent;
+import android.preference.ListPreference;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
+import android.os.UserHandle;
+import android.os.UserManager;
+import com.android.settings.own.SeekBarPreference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.preference.SwitchPreference;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-import com.android.internal.logging.MetricsLogger;
+public class LockscreenSettings extends SettingsPreferenceFragment  implements OnPreferenceChangeListener {
+    private static final String KEY_LOCKSCREEN_BLUR_RADIUS = "lockscreen_blur_radius";		
 
-public class LockscreenSettings extends SettingsPreferenceFragment
-        implements OnSharedPreferenceChangeListener {
+    private SeekBarPreference mBlurRadius;	
 
-    private static final String TAG = "LockscreenSettings";
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.own_lockscreen_settings);
+        ContentResolver resolver = getActivity().getContentResolver();
+
+
+	mBlurRadius = (SeekBarPreference) findPreference(KEY_LOCKSCREEN_BLUR_RADIUS);
+            mBlurRadius.setValue(Settings.System.getInt(resolver,
+                    Settings.System.LOCKSCREEN_BLUR_RADIUS, 14));
+            mBlurRadius.setOnPreferenceChangeListener(this);
+}
 
     @Override
     protected int getMetricsCategory() {
@@ -38,22 +61,15 @@ public class LockscreenSettings extends SettingsPreferenceFragment
     }
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        addPreferencesFromResource(R.xml.own_lockscreen_settings);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
-    }
-}
+    public boolean onPreferenceChange(Preference preference, Object newValue)
+	{
+	ContentResolver resolver = getActivity().getContentResolver();
+	 if (preference == mBlurRadius) {
+                int width = ((Integer)newValue).intValue();
+                Settings.System.putInt(resolver,
+                        Settings.System.LOCKSCREEN_BLUR_RADIUS, width);
+                return true;
+	}
+	return false;
+	}
+}	
